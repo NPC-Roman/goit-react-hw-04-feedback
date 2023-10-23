aa00cb2f1ab9cbc76a3b24f17f799dcc5b9c9de3
NPC-Roman <128088564+NPC-Roman@users.noreply.github.com>
2023-10-23 17:53:58 +0300

    change03

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+/*import React, { useState } from 'react';
 import Statistics from './Statistics';
 import Notification from './Notification';
 import Section from './Section';
@@ -46,5 +46,77 @@ function App() {
     </div>
   );
 }
+
+export default App;*/
+import { useState } from 'react';
+import Statistics from './Statistics';
+import Notification from './Notification';
+import Section from './Section';
+import FeedbackOptions from './FeedbackOptions';
+import css from './FeedbackOptions/FeedbackOptions.module.css';
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const onLeaveFeedback = rating => {
+    switch (rating) {
+      case 'good':
+        setGood(prev => prev + 1);
+        break;
+
+      case 'neutral':
+        setNeutral(prev => prev + 1);
+        break;
+
+      case 'bad':
+        setBad(prev => prev + 1);
+        break;
+
+      default:
+        return;
+    }
+  };
+
+  const countTotalFeedback = () => {
+    return good + neutral + bad;
+  };
+
+  const countPositiveFeedbackPercentage = () => {
+    const totalFeedback = countTotalFeedback();
+    return Math.round((good / totalFeedback) * 100);
+  };
+
+  const total = countTotalFeedback();
+  const positive = countPositiveFeedbackPercentage();
+
+  return (
+    <div className={css.container}>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={['good', 'neutral', 'bad']}
+          onLeaveFeedback={onLeaveFeedback}
+        />
+      </Section>
+
+      {total === 0 ? (
+        <Notification message="There is no feedback" />
+      ) : (
+        <>
+          <Section title="Statistics">
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={total}
+              positivePercentage={positive}
+            />
+          </Section>
+        </>
+      )}
+    </div>
+  );
+};
 
 export default App;
